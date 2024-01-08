07575a0b1ecb9b265bb5b350de9534666bdca669
wk <jj@jjs-MacBook-Pro.local>
2024-01-08 23:35:29 +0300

    Initial commit with spec file

---
+++ src/test/java/org/example/employeedemo/EmployeeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.employeedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
