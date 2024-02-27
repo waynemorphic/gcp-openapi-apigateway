7b4f76157f3178f56f958eaa4000b54a6b56c6f2
waynemorphic <wayneklyde@gmail.com>
2024-02-27 18:10:02 +0300

    :hammer: Fixes and updates

--- src/main/java/org/example/employeedemo/EmployeeController.java
+++ src/main/java/org/example/employeedemo/EmployeeController.java
@@ -23,20 +23,19 @@ public class EmployeeController implements EmployeeApi {
 
     @Override
     public ResponseEntity<HttpCreatedResponse> createEmployee(EmployeeData employeeData) {
-        employeeService.postEmployee(toEmployeeData(employeeData));
-        log.info("=====> Created an employee {}", toEmployeeData(employeeData));
+        employeeService.postEmployee(toEmployeeEntity(employeeData)); // b.
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @Override
     public ResponseEntity<List<EmployeeData>> getEmployeeById(String employeeId) {
         List<EmployeeEntity> list = employeeService.getEmployeeById(Long.parseLong(employeeId));
-        List<EmployeeData> listData = list.stream().map(data -> toEmployeeEntity(data)).collect(Collectors.toList());
+        List<EmployeeData> listData = list.stream().map(data -> toEmployeeData(data)).collect(Collectors.toList()); // c.
         log.info("=====> Fetched an employee {}", listData.stream().toList());
         return new ResponseEntity<>(listData, HttpStatus.OK);
     }
 
-    public EmployeeData toEmployeeEntity(EmployeeEntity employeeEntity){
+    public EmployeeData toEmployeeData(EmployeeEntity employeeEntity){ // d.
         EmployeeData employeeData = new EmployeeData();
         employeeData.setEmployeeId(Integer.valueOf(String.valueOf(employeeEntity.getEmployeeId())));
         employeeData.setFirstName(employeeEntity.getFirstName());
@@ -45,7 +44,7 @@ public class EmployeeController implements EmployeeApi {
         return employeeData;
     }
 
-    public EmployeeEntity toEmployeeData(EmployeeData employeeData){
+    public EmployeeEntity toEmployeeEntity(EmployeeData employeeData){ // e.
         EmployeeEntity employeeEntity = new EmployeeEntity();
         employeeEntity.setEmployeeId(Long.valueOf(employeeData.getEmployeeId()));
         employeeEntity.setFirstName(employeeData.getFirstName());
